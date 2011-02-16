89f867e0b5241e1d9b82c8c71471fc5c9b759355
dcheng <diana.cheng@vodafone.com>
2011-02-16 15:27:14 +0100

    change the private key to the openfire console properties and added the gitignore

--- src/java/org/onesocialweb/openfire/registration/servlet/ReCaptchaServlet.java
+++ src/java/org/onesocialweb/openfire/registration/servlet/ReCaptchaServlet.java
@@ -1,8 +1,6 @@
 package org.onesocialweb.openfire.registration.servlet;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -14,29 +12,45 @@ import net.tanesha.recaptcha.ReCaptchaImpl;
 import net.tanesha.recaptcha.ReCaptchaResponse;
 
 import org.jivesoftware.admin.AuthCheckFilter;
+import org.jivesoftware.util.JiveGlobals;
 
 @SuppressWarnings("serial")
 public class ReCaptchaServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) {				
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {	
+		
+		if (readPrivateKey()==null)
+			try {
+				response.sendError(HttpServletResponse.SC_FORBIDDEN,"There's been a server error. Please try again later or contact the server's admin");
+			} catch (IOException e) {
+							
+			}
 	
         if (!verifyChallenge(request))
 			try {
 				response.sendError(HttpServletResponse.SC_FORBIDDEN,"The Captcha text entered didn't match the pattern. Please try again!");
 			} catch (IOException e) {
-				// TODO Auto-generated catch block				
+				
 			}
 
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+		
+		if (readPrivateKey()==null)
+			try {
+				response.sendError(HttpServletResponse.SC_FORBIDDEN,"There's been a server error. Please try again later or contact the server's admin");
+			} catch (IOException e) {
+				
+			}
+		
 		if (!verifyChallenge(request))
 		try {
 			response.sendError(HttpServletResponse.SC_FORBIDDEN,"The Captcha text entered didn't match the pattern. Please try again!");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block				
+			
 		}
 	}
 
@@ -64,13 +78,7 @@ public class ReCaptchaServlet extends HttpServlet {
 	
 	private String readPrivateKey(){
 		String key=null;
-		Properties properties = new Properties();
-		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("captcha.properties");  
-		try {
-			properties.load(inputStream);
-			key =properties.getProperty("private.key");
-		} catch (IOException e) {
-		}
+		key=JiveGlobals.getProperty("onesocialweb.captcha.privateKey");
 		return key;
 } 
 	
